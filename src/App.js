92d44b45eef6530a3e852415a17c859b7dcb36ff
Jaimin <jaimin.bhojani@etatvasoft.com>
2024-12-02 11:50:33 +0530

    final change

--- src/App.js
+++ src/App.js
@@ -2,9 +2,17 @@ import React, { useState, useEffect } from "react";
 import Footer from "./component/Footer";
 import { Outlet } from "react-router-dom";
 import Header from "./component/Header";
+import { Provider, useDispatch } from "react-redux";
+
+import { fetchComponents } from "./redux/componentSlice";
 
 const App = () => {
+  const dispatch = useDispatch();
   const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    dispatch(fetchComponents());
+  }, [dispatch]);
 
   useEffect(() => {
     const handleScroll = () => {
@@ -18,20 +26,31 @@ const App = () => {
   }, []);
 
   return (
-    <div
-      className=" bg-cover bg-center top-0 left-0 bg-no-repeat min-h-screen"
-      style={{
-        backgroundImage: `url(${require("./assets/carousel-bkg.png")})`,
-      }}
-    >
-      {/* Header */}
+    <>
       <Header isScrolled={isScrolled} />
       <main>
         <Outlet />
       </main>
       <Footer />
-    </div>
+    </>
   );
 };
 
 export default App;
+
+{
+  /* <div className="text-darkestGreen p-6">
+<h1 className="font-poppins text-5xl">
+    VEHICLE_BAZAR_MANAGEMENT_SYSTEM
+</h1>
+<p className="font-poppins text-3xl text-darkGreen">
+    VEHICLE_BAZAR_MANAGEMENT_SYSTEM
+</p>
+<p className="font-arial text-xl text-mediumGreen">
+    VEHICLE_BAZAR_MANAGEMENT_SYSTEM
+</p>
+<p className="font-arial text-xl text-lightGreen">
+    VEHICLE_BAZAR_MANAGEMENT_SYSTEM
+</p>
+</div> */
+}
